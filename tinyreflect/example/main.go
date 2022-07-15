ef4a6e09c572a191551eb90f0e868d0e94b07edd
sago35 <sago35@gmail.com>
2022-07-15 21:02:46 +0900

    update

--- tinyreflect/example/main.go
+++ tinyreflect/example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 type S struct {
@@ -14,7 +15,24 @@ func main() {
 		A: 1,
 		B: "abcde",
 	}
+	x := *(*int)(unsafe.Pointer(&s))
+	fmt.Printf("A: %d\n", x)
+
+	y := *(*string)(unsafe.Pointer(uintptr(unsafe.Pointer(&s)) + unsafe.Offsetof(s.B)))
+	fmt.Printf("B: %s\n", y)
+
+	fmt.Printf("offset A : %d\n", unsafe.Offsetof(s.A))
+	fmt.Printf("offset B : %d\n", unsafe.Offsetof(s.B))
+
 	rt := TypeOf(s)
-	rf := rt.Field(0)
-	fmt.Println(rf.Name)
+	if false {
+		fmt.Printf("%#v\n", rt)
+		//fmt.Println(rf.Name)
+	}
+	//rf := rt.Field(0)
+
+	//{
+	//	rf := rt.Field(1)
+	//	fmt.Println(rf.Name)
+	//}
 }
